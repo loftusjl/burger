a2df73862d3fbdf7b1d4ad481e2cbe438913492a
Jesse Loftus <jesse.loftus@gmail.com>
2018-08-16 10:07:06 -0700

    added materialize and fontawesome

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -1,5 +1,15 @@
 // Make sure we wait to attach our handlers until the DOM is fully loaded.
 $(function() {
+
+  $(document).ready(function () {
+    $('.sidenav').sidenav();
+    $('.materialboxed').materialbox();
+    $('.parallax').parallax();
+    $('.tooltipped').tooltip();
+    $('.fixed-action-btn').floatingActionButton({
+      toolbarEnabled: true
+    });
+  })
   $(".change-burDevoured").on("click", function(event) {
     var id = $(this).data("id");
     var newburDevoured = $(this).data("newburdevoured");
