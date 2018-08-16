2cd879ebd4e2a955f759d6cb93f8c848daafa496
Jesse Loftus <jesse.loftus@gmail.com>
2018-08-15 18:27:48 -0700

    added burger images

--- db/schema.sql
+++ db/schema.sql
@@ -2,9 +2,9 @@ USE dsd7eid1r7hjlvrk;
 
 DROP TABLE IF EXISTS burgers;
  CREATE TABLE burgers(
-    burID INT AUTO_INCREMENT NOT NULL,
+    id INT AUTO_INCREMENT NOT NULL,
     burName VARCHAR(255),
     burDevoured BOOLEAN DEFAULT 0,
     burPhoto VARCHAR(255),
-    PRIMARY KEY(burID)
+    PRIMARY KEY(id)
  );
